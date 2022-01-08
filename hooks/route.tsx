3dc6a0728a0322bfd3c2a1fed3c5b04d64f31560
dev-bahar <71785467+dev-bahar@users.noreply.github.com>
2022-01-08 15:03:29 +0330

    Update route.tsx

--- hooks/route.tsx
+++ hooks/route.tsx
@@ -4,6 +4,7 @@ export function withAuth<P>(WrappedComponent:React.ComponentType<P>) {
     const  ComponentWithAuth=(props:P)=> {
        //some code
         //if user logged In
-           return <WrappedComponent {...props} />      
+           return <WrappedComponent {...props} /> 
+           //else redirect user to login page
    }
 }
